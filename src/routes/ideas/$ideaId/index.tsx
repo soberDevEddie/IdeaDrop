37545b96e925fe22df46feca4ee9402decdd6c78
EddieO23 <eocampo52@gmail.com>
2025-10-30 14:46:31 -0500

    worked on Loader for fetching api from backend using params

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+
+const fetchIdea = async (ideaId: string) => {
+  const res = await fetch(`http://localhost:8000/ideas/${ideaId}`);
+
+  if (!res.ok) {
+    throw new Error('failed to fetch data');
+  }
+
+  return res.json();
+};
 
 export const Route = createFileRoute('/ideas/$ideaId/')({
   component: IdeaDetailsPage,
-})
+  loader: async ({ params }) => {
+    return fetchIdea(params.ideaId);
+  },
+});
 
 function IdeaDetailsPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>
+  const idea = Route.useLoaderData();
+
+  return <div>{idea.title}</div>;
 }
