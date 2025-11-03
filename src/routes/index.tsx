5cefe6275c5e701d69d3e23d72adea088bffcda2
EddieO23 <eocampo52@gmail.com>
2025-11-03 15:22:59 -0600

    added IdeaCard component. Added clsx dynamic classes.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Lightbulb } from 'lucide-react';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 
 import { fetchIdeas } from '@/api/ideas';
+import IdeaCard from '@/components/IdeaCard';
 
 const ideasQueryOptions = queryOptions({
   queryKey: ['ideas'],
@@ -38,25 +39,11 @@ function App() {
         <h2 className='text-2xl font-semibold mb-4 text-gray-800'>
           Latest Ideas
         </h2>
-        <ul className='space-y-6'>
+        <div className='space-y-6'>
           {latestIdeas.map((idea) => (
-            <li
-              key={idea.id}
-              className='border border-gray-300 rounded-lg shadow p-4 bg-white'
-            >
-              <h3 className='text-lg font-bold text-gray-900'>{idea.title}</h3>
-              <p className='text-gray-600 mb-2'>{idea.summary}</p>
-              <Link
-                to='/ideas/$ideaId'
-                params={{ ideaId: idea.id.toString() }}
-                className='text-blue-600 hover:underline'
-              >
-                {' '}
-                Read more →{' '}
-              </Link>
-            </li>
+            <IdeaCard key={idea.id} idea={idea} button={false} />
           ))}
-        </ul>
+        </div>
 
         <div className='mt-6'>
           <a
