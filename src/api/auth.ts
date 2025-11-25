915cacfa91ca704d657dbbcb7bebb7bee4c42e2c
EddieO23 <eocampo52@gmail.com>
2025-11-25 17:19:35 -0600

    added logout functionality to app. user successfully able to logout

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -36,3 +36,12 @@ export const loginUser = async (credentials: {
     throw new Error(message);
   }
 };
+
+export const logoutUser = async () => {
+  try {
+    await api.post('/auth/logout');
+  } catch (err: any) {
+    const message = err.response?.data?.message || 'Failed to logout user';
+    throw new Error(message);
+  }
+};
